485110d7a65ebefa6db4ed9ec6da82e2b1f40a64
Dawid2187 <dawid.bialas2003@gmail.com>
2024-04-18 11:20:31 +0200

    Wip: added a function to remove user

--- utils/crud.py
+++ utils/crud.py
@@ -2,3 +2,19 @@ def show_users(user_list: list[dict]) -> None:
     for user in user_list:
         print(f"Twój znajomy {user['name']} opublikował: {user['posts']}")
 
+def add_new_user(users: list) -> None:
+    Kogo_szukasz = input("Podaj imie znajomego:")
+    for user in users:
+        if user['name'] == Kogo_szukasz:
+            print(user)
+def search_user(users: list) -> None:
+    Kogo_szukasz = input("Podaj imie znajomego:")
+    for user in users:
+        if user['name'] == Kogo_szukasz:
+            print(user)
+def remove_user(users)-> None:
+    Kogo_szukasz=input("Podaj imie znajomego:")
+    for user in users:
+        if user['name']==Kogo_szukasz:
+            users.remove(user)
+            print(users)

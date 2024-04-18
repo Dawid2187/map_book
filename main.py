0d601e468125473a550ebca06d7054d1c322072e
Dawid2187 <dawid.bialas2003@gmail.com>
2024-04-18 11:25:00 +0200

    Revert "Wip: added a function to create new user"
    This reverts commit 7b0f29ae

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from models.data import users
-from utils.crud import show_users, add_new_user, search_user, remove_user
-
+from utils.crud import show_users
+from utils.crud import add_new_user
 
 
 
@@ -11,8 +11,7 @@ if __name__ == "__main__":
         print("MENU:")
         print("0. Zakończ program")
         print("1. Wyświetl co u znajomych:")
-        print("3. Znajdz uzytkownika:")
-        print("4. Usuń użytkownika:")
+        print("2. Dodaj użytkownika:")
         menu_option: str = input("Dokonaj wyboru:")
         if menu_option == "0":
             print("Program kończy pracę")
@@ -21,7 +20,3 @@ if __name__ == "__main__":
             show_users(users)
         if menu_option == "2":
             add_new_user(users)
-        if menu_option == "3":
-            search_user(users)
-        if menu_option == "4":
-            remove_user(users)
--- utils/crud.py
+++ utils/crud.py
@@ -2,19 +2,9 @@ def show_users(user_list: list[dict]) -> None:
     for user in user_list:
         print(f"Twój znajomy {user['name']} opublikował: {user['posts']}")
 
-def add_new_user(users: list) -> None:
-    Kogo_szukasz = input("Podaj imie znajomego:")
-    for user in users:
-        if user['name'] == Kogo_szukasz:
-            print(user)
-def search_user(users: list) -> None:
-    Kogo_szukasz = input("Podaj imie znajomego:")
-    for user in users:
-        if user['name'] == Kogo_szukasz:
-            print(user)
-def remove_user(users)-> None:
-    Kogo_szukasz=input("Podaj imie znajomego:")
-    for user in users:
-        if user['name']==Kogo_szukasz:
-            users.remove(user)
-            print(users)
+def add_new_user(users:list)->None:
+    new_name: str = input("Enter your name: ")
+    new_surname: str = input("Enter your surname: ")
+    new_posts: int = int(input("Enter your posts: "))
+    new_user: dict = {"name": new_name, "surname": new_surname, "posts": new_posts}
+    users.append(new_user)

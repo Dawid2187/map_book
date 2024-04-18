7b0f29ae6a4df5f4323fe6f40ac7a4a345cd6d8b
Dawid2187 <dawid.bialas2003@gmail.com>
2024-04-18 10:49:23 +0200

    Wip: added a function to create new user

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from models.data import users
 from utils.crud import show_users
-
+from utils.crud import add_new_user
 
 
 
@@ -9,10 +9,14 @@ if __name__ == "__main__":
     print("Witaj użytkowniku")
     while True:
         print("MENU:")
+        print("0. Zakończ program")
         print("1. Wyświetl co u znajomych:")
+        print("2. Dodaj użytkownika:")
         menu_option: str = input("Dokonaj wyboru:")
         if menu_option == "0":
             print("Program kończy pracę")
             break
         if menu_option == "1":
             show_users(users)
+        if menu_option == "2":
+            add_new_user(users)

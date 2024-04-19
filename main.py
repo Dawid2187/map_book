148258e05f7fdadac3d5e773c3d8c836c7f8671a
Dawid2187 <dawid.bialas2003@gmail.com>
2024-04-19 15:52:38 +0200

    Wip: added a function to remove user, search user

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from models.data import users
 from utils.crud import show_users
 from utils.crud import add_new_user
+from utils.crud import remove_user
+from utils.crud import search_user
 
 
 
@@ -12,6 +14,8 @@ if __name__ == "__main__":
         print("0. Zakończ program")
         print("1. Wyświetl co u znajomych:")
         print("2. Dodaj użytkownika:")
+        print("3. Usuń użytkownika:")
+        print("4. Znajdz użytkownika:")
         menu_option: str = input("Dokonaj wyboru:")
         if menu_option == "0":
             print("Program kończy pracę")
@@ -20,3 +24,7 @@ if __name__ == "__main__":
             show_users(users)
         if menu_option == "2":
             add_new_user(users)
+        if menu_option == "3":
+            remove_user(users)
+        if menu_option == "4":
+            search_user(users)
--- utils/crud.py
+++ utils/crud.py
@@ -8,3 +8,16 @@ def add_new_user(users:list)->None:
     new_posts: int = int(input("Enter your posts: "))
     new_user: dict = {"name": new_name, "surname": new_surname, "posts": new_posts}
     users.append(new_user)
+
+def remove_user(users)-> None:
+    Kogo_szukasz=input("Podaj imie znajomego:")
+    for user in users:
+        if user['name']==Kogo_szukasz:
+            users.remove(user)
+            print(users)
+
+def search_user(users)->None:
+    kogo_szukasz=input("Podaj imie znajomego:")
+    for user in users:
+        if user['name']==kogo_szukasz:
+            print(user)

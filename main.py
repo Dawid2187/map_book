cb74abe1e1d3d3d55d676a71700685f37636e49f
Dawid2187 <dawid.bialas2003@gmail.com>
2024-05-23 17:15:57 +0200

    Wip: marker managment

--- main.py
+++ main.py
@@ -1,18 +1,29 @@
 from models.data import users
-from utils.crud import show_users
-
-
+from utils.crud import show_users,add_new_user,search_user,remove_user,update_user
 
 
 
 if __name__ == "__main__":
     print("Witaj użytkowniku")
     while True:
-        print("MENU:")
-        print("1. Wyświetl co u znajomych:")
+        print("Menu:")
+        print("0. Zakończ program")
+        print("1. Wyswietl co u znajomych")
+        print("2. Dodaj użytkownika")
+        print("3. Znajdź użytkownika")
+        print("4. Usuń użytkownika")
+        print("5. Modyfikuj użytkownika")
         menu_option: str = input("Dokonaj wyboru:")
         if menu_option == "0":
             print("Program kończy pracę")
             break
         if menu_option == "1":
             show_users(users)
+        if menu_option == "2":
+            add_new_user(users)
+        if menu_option == "3":
+            search_user(users)
+        if menu_option == "4":
+            remove_user(users)
+        if menu_option == "5":
+            update_user(users)
